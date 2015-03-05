31aca9e697a5c8efed11cd1809a16bb53202c8f1
Ben Straub <benstraub28@gmail.com>
2015-03-05 12:31:47 -0500

    removed border

--- Plot_4.R
+++ Plot_4.R
@@ -65,7 +65,7 @@ lines(hpc_subset$date_time, hpc_subset$Sub_metering_3, type="l",
       col="blue")
 legend("topright", col=c("black","red","blue"), c("Sub_metering_1", 
       "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), lwd=c(1,1), 
-       cex = 0.6)
+       cex = 0.6, bty ="n")
 
 #plot (2, 2)
 plot(hpc_subset$date_time, hpc_subset$Global_reactive_power,  type="l", 
